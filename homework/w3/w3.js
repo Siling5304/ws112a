d91cb2b744a73c1e175ef9aa2b44208121f5f079
Siling5304 <s111110525@student.nqu.edu.tw>
2023-10-20 09:46:35 +0800

    add w3.js

--- homework/w3/w3.js
+++ homework/w3/w3.js
@@ -1,49 +1,46 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import * as render from './render.js'
 
-const contacts = [
-  {id:0, name:'Mandy', tel:'0984567846'},
-  {id:1, name:'Keven', tel:'0964594135'}
+const people = [
+  {id:0, name:'Mandy', phone:'0123456789'}
 ];
 
 const router = new Router();
 
 router.get('/', list)
-  .get('/contact/new', add)
-  .get('/contact/:id', show)
-  .post('/contact', create);
+  .get('/person/new', add)
+  .get('/person/:id', show)
+  .post('/person', create);
 
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 async function list(ctx) {
-  ctx.response.body = await render.list(contacts);
+  ctx.response.body = await render.list(people);
 }
 
 async function add(ctx) {
-  ctx.response.body = await render.newcontact();
+  ctx.response.body = await render.newPerson();
 }
 
 async function show(ctx) {
   const id = ctx.params.id;
-  const contact = contacts[id];
-  if (!contact) ctx.throw(404, 'invalid contact id');
-  ctx.response.body = await render.show(contact);
+  const preson = people[id];
+  if (!preson) ctx.throw(404, 'invalid preson id');
+  ctx.response.body = await render.show(preson);
 }
 
 async function create(ctx) {
-  const body = ctx.request.body();
+  const body = ctx.request.body()
   if (body.type === "form") {
-    const pairs = await body.value();
-    const contact = {};
+    const pairs = await body.value
+    const preson = {}
     for (const [key, value] of pairs) {
-      contact[key] = value;
+      preson[key] = value
     }
-    console.log('contact=', contact)
-    const id = contacts.push(contact) - 1;
-    contact.created_at = new Date();
-    contact.id = id;
+    const id = people.push(preson) - 1;
+    preson.id = id;
     ctx.response.redirect('/');
   }
 }
